3ff9e22254d24eaa2cfe548c427b6e886b734b0b
Mark <super16mail@gmail.com>
2022-11-27 11:37:19 +0300

    Configure deploy for GitHub Actions

---
+++ scripts/gh-pages-deploy.mjs
@@ -0,0 +1,17 @@
+
+import { execa } from 'execa';
+
+(async () => {
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages']);
+    await execa('npm', ['run', 'docs:build']);
+    await execa('git', ['--work-tree', 'docs/.vitepress/dist', 'add', '--all']);
+    await execa('git', ['--work-tree', 'docs/.vitepress/dist', 'commit', '-m', 'gh-pages deployment']);
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force']);
+    await execa('rm', ['-r', 'docs/.vitepress/dist']);
+    await execa('git', ['checkout', '-f', 'main']);
+    await execa('git', ['branch', '-D', 'gh-pages']);
+  } catch (e) {
+    process.exit(1);
+  }
+})();
